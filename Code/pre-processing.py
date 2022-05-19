676ae19f1012e19b9c3e8ea38f3b1333eb3c3b9d
Rogier <roger.nagelkerken@live.nl>
2022-05-19 12:03:07 +0200

    date manipulations

--- Code/pre-processing.py
+++ Code/pre-processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
 df = pd.read_csv(r'D:\Master Thesis\autoencoder-IVS\Data\option data.csv')
-df = df.to_numpy
-print(df[0:10,0:10])
+df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
+df['exdate'] = pd.to_datetime(df['exdate'], format='%Y%m%d')
+df['daystoex'] = df['exdate']-df['date']  # Might need to be changed to trading days only
