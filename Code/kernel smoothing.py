00a5edc9d28a57d2f7c9f16996dccb5bf38bbe29
Rogier <roger.nagelkerken@live.nl>
2022-07-04 12:36:19 +0200

    First version of Kernel Smoother

--- Code/kernel smoothing.py
+++ Code/kernel smoothing.py
@@ -18,5 +18,44 @@ df['exdate'] = pd.to_datetime(df['exdate'], format='%Y-%m-%d')
 
 tenors = [10, 21, 63, 126, 189, 252]
 moneyness = [0.9, 0.95, 1, 1.05, 1.1, 1.2, 1.3]
+gridsize = len(tenors)*len(moneyness)
 
 dates = df.date.unique()
+t = df.t.unique()
+
+grid = np.meshgrid(tenors, moneyness)
+daystoex = np.tile(grid[0].flatten(), len(t))
+moneyness = np.tile(grid[1].flatten(), len(t))
+
+
+dates = np.repeat(dates, gridsize)
+t = np.repeat(t, gridsize)
+# Balanced dataframe df_bal
+df_bal = pd.DataFrame()
+df_bal['Date'] = dates
+df_bal['t'] = t
+df_bal['daystoex'] = daystoex
+df_bal['moneyness'] = moneyness
+# 'normalize' tenor and moneyness for equal importance in the kernel smoother
+df_bal['tau_nor'] = (daystoex - 10)/242
+df_bal['k_nor'] = (moneyness - 0.9)/0.4
+
+IV = np.zeros(df_bal.shape[0])
+# b is the smoothing parameter
+b=1
+for _ in tqdm(range(df_bal.shape[0]), desc='option'):
+    if _==0 or not df_bal.t[_] == df_bal.t[_-1]:
+        temp = df[df.t == df_bal.t[_]].reset_index(drop=True)
+        temp['tau_nor'] = (temp.daystoex - 10)/242
+        temp['k_nor'] = (temp.moneyness - 0.9)/0.4
+    tau_temp = np.repeat(df_bal.tau_nor[_], temp.shape[0])
+    k_temp = np.repeat(df_bal.k_nor[_], temp.shape[0])
+
+    euc_sq = (tau_temp - temp.tau_nor)**2 + (k_temp - temp.k_nor)**2
+    # kernel is the weight that each point gets
+    kernel = np.exp(- euc_sq/(2*(b**2)))
+    IV[_] = np.dot(temp.IV,kernel) / np.sum(kernel)
+
+df_bal['IV'] = IV
+
+
