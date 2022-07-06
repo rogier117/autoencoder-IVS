3102fec1e3fcf0282ccefa985a1e449f95f27e94
Rogier <roger.nagelkerken@live.nl>
2022-07-06 14:55:17 +0200

    First version of PCA

---
+++ Code/PCA.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+# importing or loading the dataset
+dataset = pd.read_csv(r'D:\Master Thesis\autoencoder-IVS\Data\X balanced.csv')
+
+# distributing the dataset into two components X and Y
+X = dataset.values
+# Splitting the X and Y into the
+# Training set and Testing set
+
+
+X_train, X_test = train_test_split(X, test_size = 0.15, shuffle=False)
+# performing preprocessing part
+
+sc = StandardScaler()
+
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+# Applying PCA function on training
+# and testing set of X component
+
+
+pca = PCA(n_components = 3)
+
+f_train = pca.fit_transform(X_train)
+f_test = pca.transform(X_test)
+
+explained_variance = pca.explained_variance_ratio_
