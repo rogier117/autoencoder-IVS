83f798c7a1a643dc7a3b444875e7ae19e51f6f18
Rogier <roger.nagelkerken@live.nl>
2022-07-26 12:28:21 +0200

    Change to functions

--- Code/AE.py
+++ Code/AE.py
@@ -15,52 +15,77 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import layers
 
+
+def ae_preprocessing(X, df, split=0.8):
+    X = np.array(X)
+    y = X.flatten()
+    moneyness = np.array(df.moneyness)
+    tenor = np.array(df.daystoex)
+    char = np.append(moneyness[:, None], tenor[:, None], axis=1)
+    X = np.repeat(X, X.shape[1], axis=0)
+    cut_off = df.t.unique()[round(len(df.t.unique()) * split)]
+    X_train = X[df.t <= cut_off, :]
+    X_test = X[df.t > cut_off, :]
+    char_train = char[df.t <= cut_off, :]
+    char_test = char[df.t > cut_off, :]
+    y_train = y[df.t <= cut_off]
+    y_test = y[df.t > cut_off]
+    return X_train, X_test, char_train, char_test, y_train, y_test
+
+
+def create_model(n_factors=3, encoder_width=21, decoder_width=21):
+    # This is the size of our encoded representations
+    encoding_dim = n_factors
+
+    # This is our input image
+    input_1 = keras.Input(shape=(42,))
+    input_2 = keras.Input(shape=(2,))
+    # "encoded" is the encoded representation of the input
+    encoded = layers.Dense(encoder_width, activation='relu')(input_1)
+
+    encoded2 = layers.Dense(encoding_dim, activation='relu')(encoded)
+    # Add inputs to decoder
+    merge = layers.Concatenate()([encoded2, input_2])
+    # "decoded" is the lossy reconstruction of the input
+    decoded = layers.Dense(decoder_width, activation='relu')(merge)
+
+    decoded2 = layers.Dense(1, activation='sigmoid')(decoded)
+
+    # This model maps an input to its reconstruction
+    autoencoder = keras.Model(inputs=[input_1, input_2], outputs=decoded2)
+
+    # This model maps an input to its encoded representation
+    # encoder = keras.Model(input_img, encoded)
+    #
+    # # This is our encoded (32-dimensional) input
+    # encoded_input = keras.Input(shape=(encoding_dim,))
+    # # Retrieve the last layer of the autoencoder model
+    # decoder_layer = autoencoder.layers[-1]
+    # # Create the decoder model
+    # decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
+    return autoencoder
+
+
+def train_ae(model, X_train, X_test, char_train, char_test, y_train, y_test, epochs=10, batch_size=42):
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit([X_train, char_train], y_train,
+              epochs=epochs,
+              batch_size=batch_size,
+              shuffle=True,
+              validation_data=([X_test, char_test], y_test))
+    return model
+
+
+def test_ae(model, X_test, char_test):
+    y_hat = model.predict([X_test, char_test])
+    return y_hat
+
+
 X = pd.read_csv(r'D:\Master Thesis\autoencoder-IVS\Data\X balanced.csv')
-X = np.array(X)
 df = pd.read_csv(r'D:\Master Thesis\autoencoder-IVS\Data\option data balanced.csv')
 
-y = X.flatten()
-
-moneyness = np.array(df.moneyness)
-tenor = np.array(df.daystoex)
-char = np.append(moneyness[:,None], tenor[:,None], axis=1)
-
-X = np.repeat(X, X.shape[1], axis=0)
-
-
-# This is the size of our encoded representations
-encoding_dim = 3  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
-
-# This is our input image
-input = keras.Input(shape=(42,))
-input_2 = keras.Input(shape=(2,))
-# "encoded" is the encoded representation of the input
-encoded = layers.Dense(21, activation='relu')(input)
-
-encoded2 = layers.Dense(encoding_dim, activation='relu')(encoded)
-# Add inputs to decoder
-merge = layers.Concatenate()([encoded2, input_2])
-# "decoded" is the lossy reconstruction of the input
-decoded = layers.Dense(21, activation='relu')(merge)
-
-decoded2 = layers.Dense(1, activation='sigmoid')(decoded)
-
-# This model maps an input to its reconstruction
-autoencoder = keras.Model(inputs=[input, input_2], outputs=decoded2)
-
-# # This model maps an input to its encoded representation
-# encoder = keras.Model(input_img, encoded)
-#
-# # This is our encoded (32-dimensional) input
-# encoded_input = keras.Input(shape=(encoding_dim,))
-# # Retrieve the last layer of the autoencoder model
-# decoder_layer = autoencoder.layers[-1]
-# # Create the decoder model
-# decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
-
-autoencoder.compile(optimizer='adam', loss='mean_squared_error')
-
-autoencoder.fit([X, char], y,
-                epochs=3,
-                batch_size=42,
-                shuffle=True,)
+X_train, X_test, char_train, char_test, y_train, y_test = ae_preprocessing(X=X, df=df, split=0.8)
+model = create_model(n_factors=3, encoder_width=21, decoder_width=21)
+trained_model = train_ae(model=model, X_train=X_train, X_test=X_test, char_train=char_train, char_test=char_test,
+                         y_train=y_train, y_test=y_test, epochs=5, batch_size=42)
+y_hat = test_ae(model=trained_model, X_test=X_test, char_test=char_test)
